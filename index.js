5b6c6703f85124d2fe1e0f3e57ae79e67aeda7d2
Shaheer <ahmedshaheer605@gmail.com>
2025-09-13 12:23:00 +0500

    initialize git to project

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import bodyParser from "body-parser";
+
+const app = express();
+
+var titles = []
+var articles = []
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/", (req, res)=>{
+    res.render("index.ejs", {
+        headings: titles,
+        paras: articles,
+    })
+
+})
+
+app.get("/post", (req, res)=>{
+    res.render("post.ejs")
+})
+
+app.post("/submit", (req, res)=>{
+    titles.push(req.body["title"])
+    articles.push(req.body["content"])
+    res.redirect("/")
+})
+
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000")
+})
